58f9d1a358648392e1dcd2dfce838360626a44dd
Iraqez <vladimir.gumenyuk@gmail.com>
2015-12-24 03:23:14 +0200

    1

--- parserdzk/mytest.py
+++ parserdzk/mytest.py
@@ -4,6 +4,7 @@ koatuu = '6810100000'
 
 d = []
 
+#koatuu:zona:kvadrat:parcel
 for zona in ['{0:02d}'.format(i) for i in range(1,20)]:
     for kvartal in ['{0:03d}'.format(i) for i in range(1,20)]:
         for parcel in ['{0:04d}'.format(i) for i in range(1,200)]:
