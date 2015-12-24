543949a0188d15a00cfd966c9d495bb7891a0df0
Iraqez <vladimir.gumenyuk@gmail.com>
2015-12-24 02:20:21 +0200

    1

--- parserdzk/mytest.py
+++ parserdzk/mytest.py
@@ -1,27 +1,13 @@
 from parserdzk import datacadnum
 
-koatuu = 6810100000
+koatuu = '6810100000'
 
 d = []
 
-for i in ['{0:04d}'.format(i) for i in range(1,1000)]:
-    d.append('6810100000:11:003:'+i)
-
-
-# d = [
-#     '6810100000:11:003:0101',
-#     '6810100000:11:003:0102',
-#     '6810100000:11:003:0103',
-#     '6810100000:11:003:0104',
-#     '6810100000:11:003:0105',
-#     '6810100000:11:003:0106',
-#     '6810100000:11:003:0107',
-#     '6810100000:11:003:0108',
-#     '6810100000:11:003:0109',
-#     '6810100000:11:003:0110',
-# ]
-
-
+for zona in ['{0:02d}'.format(i) for i in range(1,20)]:
+    for kvartal in ['{0:03d}'.format(i) for i in range(1,20)]:
+        for parcel in ['{0:04d}'.format(i) for i in range(1,200)]:
+            d.append(str(koatuu+':'+zona+':'+kvartal+":"+parcel))
 
 for i in d:
     try:
@@ -30,8 +16,5 @@ for i in d:
         else:
             raise TypeError
     except TypeError:
-        pass
-#
-# d = [x+1 for x in d]
-#
-# lst = ['{0:04d}'.format(i) for i in range(1,10000)]
+        print('Нет данных по участку: ' + i)
+        # pass
