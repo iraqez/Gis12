d05bed8367d343c52dce76f30cbec33b1eacae58
Iraqez <vladimir.gumenyuk@gmail.com>
2015-12-27 09:35:12 +0200

    Добавление выборки в базу

--- parserdzk/mytest2.py
+++ parserdzk/mytest2.py
@@ -2,22 +2,33 @@ from parserdzk import datacadnum
 
 koatuu = '6810100000'
 
-d = []
+
 
 def cadnum(koatuu):
-    for zona in ['{0:02d}'.format(i) for i in range(27,28)]:
-        for kvartal in ['{0:03d}'.format(i) for i in range(1,3)]:
-            for parcel in ['{0:04d}'.format(i) for i in range(1,50)]:
-                d.append(str(koatuu+':'+zona+':'+kvartal+":"+parcel))
+    nums = []
+    notnum = 0
+    cnums = []
 
-    for i in d:
+    for zona in ['{0:02d}'.format(i) for i in range(1,3)]:
+        for kvartal in ['{0:03d}'.format(i) for i in range(1,3)]:
+            for parcel in ['{0:04d}'.format(i) for i in range(1,10)]:
+                cn = [koatuu,zona,kvartal,parcel]
+                nums.append(":".join(cn))
+
+    for i in nums:
         try:
             if datacadnum.cnum(i) != None:
+                cnums.append(datacadnum.cnum(i))
                 print(datacadnum.cnum(i))
+
+                notnum = 0
             else:
                 raise TypeError
         except TypeError:
-            pass
+            notnum += 1
+            print(notnum)
+
+    return cnums
 
 if __name__ == '__main__':
-    print(cadnum(koatuu))
+    cadnum(koatuu)
