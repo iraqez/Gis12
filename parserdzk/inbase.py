65376e4543f5cb9375ef1e3531482bb3d11f20b2
Iraqez <vladimir.gumenyuk@gmail.com>
2016-01-29 16:27:30 +0200

    1

--- parserdzk/inbase.py
+++ parserdzk/inbase.py
@@ -1,21 +1,26 @@
 from psycopg2.extensions import AsIs
 import psycopg2
 from parserdzk import datacadnum
+import csv
+
+whith open('')
 
 conn = psycopg2.connect("dbname='gis12' user='postgres' host='localhost' password='workfree'")
 cur = conn.cursor()
 
 song = datacadnum.cnum('6810100000:11:003:0101')
-insert_statement = 'insert into kadastr_fromdzk (%s) values %s'
+insert_nums = 'insert into kadastr_fromdzk (%s) values %s'
 
 def cadnum(song):
     columns = song.keys()
     values = [song[column] for column in columns]
 
     cur.execute(insert_statement, (AsIs(','.join(columns)), tuple(values)))
-    conn.commit()
-    cur.close()
-    conn.close()
+
+
+conn.commit()
+cur.close()
+conn.close()
 
 
 
